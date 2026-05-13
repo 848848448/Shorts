3e3a1b560a21a7a2143d6075bb9bb085ec176207
848848448 <Avrumy5872877@gmail.com>
2026-05-13 14:53:57 -0400

    Modify Firebase config and initialization
    Updated Firebase configuration values and initialization logic.

--- config.js
+++ config.js
@@ -1,14 +1,16 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAmKpAnoV_aG_3_rGMf4TW7I9vO7W3zXk",
+  apiKey: "AIzaSyAmKpAnoV_aG_3_rGMf4l_FzcbAWzwHTnY",
   authDomain: "flutter-ai-playground-8cfec.firebaseapp.com",
   databaseURL: "https://flutter-ai-playground-8cfec-default-rtdb.firebaseio.com",
   projectId: "flutter-ai-playground-8cfec",
-  storageBucket: "flutter-ai-playground-8cfec.appspot.com",
+  storageBucket: "flutter-ai-playground-8cfec.firebasestorage.app",
   messagingSenderId: "989658103041",
   appId: "1:989658103041:web:f33b1ed6d748ef98b96f00"
 };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// צינדט אָן Firebase
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
 const db = firebase.database();
 const storage = firebase.storage();

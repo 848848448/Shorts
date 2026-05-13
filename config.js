3140f0aa49812eb09b5ceccfd2ef19d1ddfb28c4
848848448 <Avrumy5872877@gmail.com>
2026-05-13 01:32:46 -0400

    Set up Firebase configuration in config.js
    Added Firebase configuration and initialization code.

--- config.js
+++ config.js
@@ -1,1 +1,15 @@
+// Firebase Setup
+const firebaseConfig = {
+  apiKey: "לייג_דא_אריין_דיין_API_KEY",
+  authDomain: "shorts-e05cc.firebaseapp.com",
+  databaseURL: "https://shorts-e05cc-default-rtdb.firebaseio.com",
+  projectId: "shorts-e05cc",
+  storageBucket: "shorts-e05cc.appspot.com",
+  messagingSenderId: "777444333", // דיין עכטע ID
+  appId: "1:777444333:web:abcd123" // דיין עכטע App ID
+};
 
+// Initialize
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+const storage = firebase.storage();
